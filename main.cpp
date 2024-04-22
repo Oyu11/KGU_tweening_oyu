03b59586e19f2da4fb33f498363ff48e4f945a92
Oyu <131845932+Oyu11@users.noreply.github.com>
2024-04-22 16:27:41 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,63 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <cmath>
+
 float t = 0.0;
 int state = 1;
-void drawPolygon(float radius, int sides) {
-    glBegin(GL_POLYGON);
-    for (int i = 0; i < sides; i++) {
-        float angle = 2.0 * M_PI * i / sides;
+
+void drawPentagon(float radius) {
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 5; i++) {
+        float angle = 2.0 * M_PI * i / 5;
         float x = radius * cos(angle);
         float y = radius * sin(angle);
         glVertex2f(x, y);
     }
     glEnd();
 }
+void drawTwentySidedPolygon(float radius) {
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 20; i++) {
+        float angle = 2.0 * M_PI * i / 20;
+        float x = radius * cos(angle);
+        float y = radius * sin(angle);
+        glVertex2f(x, y);
+    }
+    glEnd();
+}
+
+void tween_pol(float t, float radiusA, float radiusB) {
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 20; i++) {
+        float angleA = 2.0 * M_PI * i / 5;
+        float xA = radiusA * cos(angleA);
+        float yA = radiusA * sin(angleA);
+
+        float angleB = 2.0 * M_PI * i / 20;
+        float xB = radiusB * cos(angleB);
+        float yB = radiusB * sin(angleB);
+
+        float x = (1 - t) * xA + t * xB;
+        float y = (1 - t) * yA + t * yB;
+
+        glVertex2f(x, y);
+    }
+    glEnd();
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    float radius1 = 0.1 + 0.4 * t;
-    float radius2 = 0.5 + 0.4 * t;
-    int sides = 5 + 15 * t;
-    glColor3f(1.0 - t, 0.0, t);
-    drawPolygon(radius1, sides);
-    glColor3f(t, 0.0, 1.0 - t);
-    drawPolygon(radius2, sides);
+
+    glColor3f(1.0, 0.0, 0.0);
+    drawPentagon(0.5);
+
+    glColor3f(0.0, 1.0, 0.0);
+    drawTwentySidedPolygon(0.5);
+
+    // Tweening animation
+    glColor3f(0.0, 0.0, 1.0);
+    tween_pol(t, 0.5, 0.5);
+
     switch (state) {
         case 1:
             if (t < 1) t += 0.01;
@@ -29,29 +65,29 @@ void display() {
             break;
         case -1:
             if (t > 0) t -= 0.01;
-            else state = 1;
             break;
     }
+
     glutSwapBuffers();
 }
+
 void timer(int value) {
     glutPostRedisplay();
-    glutTimerFunc(120, timer, 0);
+    glutTimerFunc(30, timer, 0);
 }
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 800);
-    glutCreateWindow("Animation");
+    glutCreateWindow("Tweening Oyu");
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glutDisplayFunc(display);
-    glutTimerFunc(60, timer, 0);
+    glutTimerFunc(20, timer, 0);
     glutMainLoop();
     return 0;
 }
-//tweening hiij bui heseg ali n ve
-//zurj bui heseg ali n ve
